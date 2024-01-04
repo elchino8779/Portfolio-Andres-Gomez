f3d0cd68e970003d592f74a41b7b8c27a6e8a730
Andres Gomez <elchino8779@gmail.com>
2024-01-04 18:02:42 -0300

    Cambios minimos de navegacion

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,25 +1,36 @@
 import './Nav.scss'
 import AvatarLentes from '/Avatares/avatar-lentes.png'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { useSelectorContext } from '../../Context/SelectorContext'
 import homeIcon from '/IconsNav/home.svg'
 import certificacionesIcon from '/IconsNav/certificate.svg'
 import proyectsIcon from '/IconsNav/proyects.svg'
 import aboutIcon from '/IconsNav/about.svg'
 import contactIcon from '/IconsNav/contact.svg'
+import vigneteImg from '/Icons/vignete.png'
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Nav = () => {
 
   const { selectorX, selectorW, handleSelector } = useSelectorContext();
+  const navigate = useNavigate()
+
+
+  const goHome = () =>{
+    navigate('/')
+  }
 
   return (
     <nav className='nav-container'>
-      <div className="logo-texts">
-        <img src={AvatarLentes} alt="Avatar" loading='eager'/>
+      <div className="logo-texts" onClick={goHome}>
+        <div className="vignete-nav-container">
+          <img src={vigneteImg} alt="Viñeta" />
+          <p>Hola!</p>
+        </div>
+        <img src={AvatarLentes} alt="Avatar" loading='eager' />
         <div className="text">
-          <h1>Andres Gomez</h1>
+          <h1>Andrés Gómez</h1>
           <h3>Web Developer Frontend</h3>
         </div>
       </div>
@@ -30,23 +41,23 @@ const Nav = () => {
 
           <NavLink className='link-nav' to='/' onClick={() => handleSelector('32px', '60px')}>
             <p>Home</p>
-            <LazyLoadImage src={homeIcon} alt="Home" effect='blur'/>
+            <LazyLoadImage src={homeIcon} alt="Home" effect='blur' />
           </NavLink>
           <NavLink className='link-nav' to='/certificaciones'>
             <p>Certificaciones</p>
-            <LazyLoadImage src={certificacionesIcon} alt="Certificaciones" effect='blur'/>
+            <LazyLoadImage src={certificacionesIcon} alt="Certificaciones" effect='blur' />
           </NavLink>
           <NavLink className='link-nav' to='/proyectos' >
             <p>Proyectos</p>
-            <LazyLoadImage src={proyectsIcon} alt="Proyectos" effect='blur'/>
+            <LazyLoadImage src={proyectsIcon} alt="Proyectos" effect='blur' />
           </NavLink>
           <NavLink className='link-nav' to='/sobremi' >
             <p>Sobre Mí</p>
-            <LazyLoadImage src={aboutIcon} alt="Sobre Mí" effect='blur'/>
+            <LazyLoadImage src={aboutIcon} alt="Sobre Mí" effect='blur' />
           </NavLink>
           <NavLink className='link-nav' to='/contacto' >
             <p>Contacto</p>
-            <LazyLoadImage src={contactIcon} alt="Contacto" effect='blur'/>
+            <LazyLoadImage src={contactIcon} alt="Contacto" effect='blur' />
           </NavLink>
         </div>
       </div>
